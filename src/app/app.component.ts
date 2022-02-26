7c00c572f2cfc728817af5d7e55e5651b68242d5
Nam Pham <pvnam95@hotmail.com>
2022-02-26 18:34:53 +0900

    Connected to real server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'home-assistant-web-client';
+  constructor(private httpClient: HttpClient) { }
+  ngOnInit() {
+    this.getWeatherForecast();
+  }
+
+  private getWeatherForecast() {
+    this.httpClient.get('https://nam-home-assistant.herokuapp.com/weatherforecast').subscribe(data => {
+      console.log(data);
+    });
+  }
 }
